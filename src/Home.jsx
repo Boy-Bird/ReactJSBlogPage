fc8a3fdc6d0089da93b7ae74582439c51778338f
Boy-Bird <yashparihar44@gmail.com>
2024-02-05 16:05:07 +0530

    State Management

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,13 @@
-import Feed from "./Feed"
-const Home = ({ posts, fetchError }) => {
+import Feed from "./Feed";
+import { useContext } from 'react';
+import DataContext from './context/DataContext';
+
+const Home = () => {
+  const { searchResults, fetchError } = useContext(DataContext);
   return (
     <main className="Home">
       {fetchError && <p className="statusMsg" style={{color: "red"}}>{fetchError}</p>}
-      {!fetchError && (posts.length ? <Feed posts={posts} /> : <p className="statusMsg">No posts to display.</p>)}
+      {!fetchError && (searchResults.length ? <Feed posts={searchResults} /> : <p className="statusMsg">No posts to display.</p>)}
     </main>
   )
 }
